a99f9889009b158376965fd2197a3aa03d40b834
tanmaykumar1993 <tanmay.kr.sarkar@live.com>
2019-11-08 09:54:20 +0530

    initial commit

---
+++ user-matching/src/main/java/com/example/usermatch/UserMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMatchingApplication.class, args);
+	}
+
+}
---
+++ user-matching/src/test/java/com/example/usermatch/UserMatchingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.usermatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserMatchingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
